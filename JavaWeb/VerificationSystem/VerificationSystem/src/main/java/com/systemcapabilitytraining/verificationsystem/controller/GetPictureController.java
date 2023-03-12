3d03df387c3f27e7daf0e8090b30745576823724
Fizz <Fizz_ncx@outlook.com>
2023-03-12 21:47:41 +0800

    web learning

--- JavaWeb/VerificationSystem/VerificationSystem/src/main/java/com/systemcapabilitytraining/verificationsystem/controller/GetPictureController.java
+++ JavaWeb/VerificationSystem/VerificationSystem/src/main/java/com/systemcapabilitytraining/verificationsystem/controller/GetPictureController.java
@@ -1,7 +1,12 @@
 package com.systemcapabilitytraining.verificationsystem.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.IOException;
 
 /**
  * ClassName: getPicture
@@ -13,6 +18,32 @@ import org.springframework.web.bind.annotation.RestController;
  * @Version 1.0
  */
 @RestController
-public class getPicture {
-    @GetMapping("/")
+public class GetPictureController {
+    @PostMapping("/upload")
+    public String up(String nickname, MultipartFile photo, HttpServletRequest request) throws IOException{
+        System.out.println(nickname);
+        System.out.println(photo.getOriginalFilename());
+        System.out.println(photo.getContentType());
+
+        //获取上下文路径
+        String path = request.getServletContext().getRealPath("/upload/");
+
+        System.out.println(path);
+
+        SaveFile(photo,path);
+        return "dudulu~,uploaded~";
+    }
+
+    public void SaveFile(MultipartFile avatar,String path) throws IOException{
+
+        //test
+        path = "C:/testPic/";
+        File dir = new File(path);
+        if(!dir.exists()) {
+            dir.mkdir();
+        }
+        File file = new File(path+avatar.getOriginalFilename());
+        avatar.transferTo((file));
+    }
+
 }
--- JavaWeb/VerificationSystem/VerificationSystem/src/main/java/com/systemcapabilitytraining/verificationsystem/controller/myController.java
+++ JavaWeb/VerificationSystem/VerificationSystem/src/main/java/com/systemcapabilitytraining/verificationsystem/controller/myController.java
@@ -1,10 +1,21 @@
-package com.systemcapabilitytraining.verificationsystem.controller;/**
-* ClassName: myController
-* Package: com.systemcapabilitytraining.verificationsystem.controller
-* Description:
-* @Author Atmsekr
-* @Create 2023/3/11 20:23
-* @Version 1.0
-*/
+package com.systemcapabilitytraining.verificationsystem.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * ClassName: myController
+ * Package: com.systemcapabilitytraining.verificationsystem.controller
+ * Description:
+ *
+ * @Author Atmsekr
+ * @Create 2023/3/11 20:23
+ * @Version 1.0
+ */
+@RestController
 public class myController {
+    @GetMapping("/dudulu")
+    public String dudulu(){
+        return "dudulu~~~";
+    }
 }

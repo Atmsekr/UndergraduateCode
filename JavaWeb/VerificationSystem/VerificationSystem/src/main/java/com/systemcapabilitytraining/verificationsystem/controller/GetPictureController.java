2f8711e9b61ae1bdb9f75ad43eeda189ab46fc56
Fizz <Fizz_ncx@outlook.com>
2023-03-12 21:47:26 +0800

    web learning

---
+++ JavaWeb/VerificationSystem/VerificationSystem/src/main/java/com/systemcapabilitytraining/verificationsystem/controller/GetPictureController.java
@@ -0,0 +1,18 @@
+package com.systemcapabilitytraining.verificationsystem.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * ClassName: getPicture
+ * Package: com.systemcapabilitytraining.verificationsystem.controller
+ * Description:
+ *
+ * @Author Atmsekr
+ * @Create 2023/3/12 15:54
+ * @Version 1.0
+ */
+@RestController
+public class getPicture {
+    @GetMapping("/")
+}
---
+++ JavaWeb/VerificationSystem/VerificationSystem/src/main/java/com/systemcapabilitytraining/verificationsystem/controller/myController.java
@@ -0,0 +1,10 @@
+package com.systemcapabilitytraining.verificationsystem.controller;/**
+* ClassName: myController
+* Package: com.systemcapabilitytraining.verificationsystem.controller
+* Description:
+* @Author Atmsekr
+* @Create 2023/3/11 20:23
+* @Version 1.0
+*/
+public class myController {
+}
